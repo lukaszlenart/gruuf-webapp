861e9ca53155aeb6951185e33343b2652d5c57d7
Lukasz Lenart <lukasz.lenart@gmail.com>
2015-07-17 15:30:06 +0200

    Adds validation

--- src/main/java/me/rbw/web/actions/bike/Add.java
+++ src/main/java/me/rbw/web/actions/bike/Add.java
@@ -1,17 +1,41 @@
 package me.rbw.web.actions.bike;
 
 import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.validator.annotations.RequiredStringValidator;
 import me.rbw.web.RbwActions;
 import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.interceptor.validation.SkipValidation;
 
 public class Add extends ActionSupport {
 
+    @SkipValidation
     public String execute() {
-        return SUCCESS;
+        return INPUT;
     }
 
     @Action("add-submit")
     public String addSubmit() {
         return RbwActions.HOME;
+    }
+
+    private String friendlyName;
+    private String vin;
+
+    public String getFriendlyName() {
+        return friendlyName;
+    }
+
+    @RequiredStringValidator(message = "'Friendly name' is required!")
+    public void setFriendlyName(String friendlyName) {
+        this.friendlyName = friendlyName;
+    }
+
+    public String getVin() {
+        return vin;
+    }
+
+    @RequiredStringValidator(message = "'VIN' is required!")
+    public void setVin(String vin) {
+        this.vin = vin;
     }
 }
