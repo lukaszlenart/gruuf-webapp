63c75c85048a0395101a7b5dfe06235be20d7352
Lukasz Lenart <lukasz.lenart@gmail.com>
2015-09-19 11:46:55 +0200

    Adds support for namespace to tiles definitions

--- src/main/java/me/rbw/web/TilesUnknownHandler.java
+++ src/main/java/me/rbw/web/TilesUnknownHandler.java
@@ -5,6 +5,8 @@ import com.opensymphony.xwork2.Result;
 import com.opensymphony.xwork2.UnknownHandler;
 import com.opensymphony.xwork2.XWorkException;
 import com.opensymphony.xwork2.config.entities.ActionConfig;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.struts2.ServletActionContext;
 import org.apache.struts2.views.tiles.TilesResult;
 import org.apache.tiles.TilesContainer;
@@ -13,8 +15,16 @@ import org.apache.tiles.access.TilesAccess;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.transaction.TransactionRequiredException;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class TilesUnknownHandler implements UnknownHandler {
+
+    private static Logger LOG = LogManager.getLogger(TilesUnknownHandler.class);
+
     @Override
     public ActionConfig handleUnknownAction(String namespace, String actionName) throws XWorkException {
         return null;
@@ -29,16 +39,46 @@ public class TilesUnknownHandler implements UnknownHandler {
 
         TilesContainer container = TilesAccess.getContainer(servletContext);
 
-        if (container.isValidDefinition(actionName, request, response)) {
-            return new TilesResult(actionName);
+        String namespace = ServletActionContext.getActionMapping().getNamespace();
+        Set<String> definitions = buildDefinitionNames(namespace, actionName, resultCode);
+
+        for (String definition : definitions) {
+            LOG.debug("Looking for tiles definition: {}", definition);
+
+            if (container.isValidDefinition(definition, request, response)) {
+                return new TilesResult(definition);
+            }
         }
 
-        String definition = actionName + "-" + resultCode;
-        if (container.isValidDefinition(definition, request, response)) {
-            return new TilesResult(definition);
-        }
-
+        LOG.debug("Couldn't find tiles definition for namespace {}, action name {} and result code {}", namespace, actionName, resultCode);
         return null;
+    }
+
+    protected Set<String> buildDefinitionNames(String namespace, String actionName, String resultCode) {
+        Set<String> definitions = new LinkedHashSet<>();
+
+        if (namespace.startsWith("/")) {
+            namespace = namespace.substring(1);
+        }
+
+        if (!Objects.equals(namespace, "") && !Objects.equals(namespace, "/")){
+            if (namespace.endsWith("/")) {
+                definitions.add(namespace + actionName);
+                definitions.add(namespace + actionName + "-" + resultCode);
+                definitions.add(namespace + resultCode);
+            } else {
+                definitions.add(namespace + "/" + actionName);
+                definitions.add(namespace + "/" + actionName + "-" + resultCode);
+                definitions.add(namespace + "/" + resultCode);
+            }
+        }
+
+        definitions.add(actionName);
+        definitions.add(actionName + "-" + resultCode);
+
+        LOG.debug("Possible definition's names {}", definitions);
+
+        return definitions;
     }
 
     @Override
