9ca51d2f2ad65c3d7b2686df3d08502713c3a454
Lukasz Lenart <lukasz.lenart@gmail.com>
2017-02-21 19:22:52 +0100

    Adds basic support for LetsEncrypt

---
+++ src/main/java/com/gruuf/web/LetsEncryptServlet.java
@@ -0,0 +1,19 @@
+package com.gruuf.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LetsEncryptServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        log("Handling LetsEncrypt request");
+        response.setContentType("text/plain");
+        response.getOutputStream().println("XXXXX");
+    }
+}
