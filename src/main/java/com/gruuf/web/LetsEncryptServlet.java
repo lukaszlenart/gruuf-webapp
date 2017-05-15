635253866ff38c94a7ccab19403305cabd8ba485
Lukasz Lenart <lukasz.lenart@gmail.com>
2017-05-15 07:55:30 +0200

    Adds LetsEncrypt challenge response

--- src/main/java/com/gruuf/web/LetsEncryptServlet.java
+++ src/main/java/com/gruuf/web/LetsEncryptServlet.java
@@ -7,13 +7,12 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class LetsEncryptServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         log("Handling LetsEncrypt request");
+        String challenge = getInitParameter("challenge");
+
         response.setContentType("text/plain");
-        response.getOutputStream().println("XXXXX");
+        response.getOutputStream().println(challenge);
     }
 }
