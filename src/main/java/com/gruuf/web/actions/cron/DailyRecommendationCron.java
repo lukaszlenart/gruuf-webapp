880f05ef781a3d4156d00da2d054bc8cc61d1fd8
Lukasz Lenart <lukasz.lenart@gmail.com>
2017-10-30 15:53:22 +0100

    Purges and handles exception

--- src/main/java/com/gruuf/web/actions/cron/DailyRecommendationCron.java
+++ src/main/java/com/gruuf/web/actions/cron/DailyRecommendationCron.java
@@ -2,6 +2,7 @@ package com.gruuf.web.actions.cron;
 
 import com.google.appengine.api.taskqueue.Queue;
 import com.google.appengine.api.taskqueue.QueueFactory;
+import com.google.appengine.api.taskqueue.TaskAlreadyExistsException;
 import com.google.appengine.api.taskqueue.TaskOptions;
 import com.gruuf.auth.Anonymous;
 import com.gruuf.model.Bike;
@@ -24,14 +25,22 @@ public class DailyRecommendationCron extends BaseAction {
 
     public String execute() {
         Queue queue = QueueFactory.getDefaultQueue();
+        
+        LOG.info("Purging old tasks");
+        queue.purge();
 
         for (Bike bike : garage.list()) {
             LOG.info("Creating daily recommendation check task for bikeId {}", bike.getId());
-            queue.add(TaskOptions.Builder
-                    .withUrl("/tasks/daily-recommendation-check")
-                    .taskName("bikeId-" + bike.getId())
-                    .param("bikeId", bike.getId())
-            );
+            try {
+                queue.add(TaskOptions.Builder
+                        .withUrl("/tasks/daily-recommendation-check")
+                        .taskName("bikeId-" + bike.getId())
+                        .param("bikeId", bike.getId())
+                );
+            } catch (TaskAlreadyExistsException e) {
+                LOG.error("Cannot generate daily recommendation check for " + bike.getId(), e);
+            }
+
         }
 
         return SUCCESS;
