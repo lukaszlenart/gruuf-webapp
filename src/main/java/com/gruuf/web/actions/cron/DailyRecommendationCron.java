b9addc5128eb0241f7a17258f67e41abad8fd401
Lukasz Lenart <lukasz.lenart@gmail.com>
2017-11-01 17:10:52 +0100

    Tries to delete existing tasks

--- src/main/java/com/gruuf/web/actions/cron/DailyRecommendationCron.java
+++ src/main/java/com/gruuf/web/actions/cron/DailyRecommendationCron.java
@@ -3,6 +3,7 @@ package com.gruuf.web.actions.cron;
 import com.google.appengine.api.taskqueue.Queue;
 import com.google.appengine.api.taskqueue.QueueFactory;
 import com.google.appengine.api.taskqueue.TaskAlreadyExistsException;
+import com.google.appengine.api.taskqueue.TaskHandle;
 import com.google.appengine.api.taskqueue.TaskOptions;
 import com.gruuf.auth.Anonymous;
 import com.gruuf.model.Bike;
@@ -11,6 +12,7 @@ import com.gruuf.web.actions.BaseAction;
 import com.opensymphony.xwork2.inject.Inject;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.message.ParameterizedMessage;
 import org.apache.struts2.convention.annotation.Result;
 
 import static com.opensymphony.xwork2.Action.SUCCESS;
@@ -31,14 +33,21 @@ public class DailyRecommendationCron extends BaseAction {
 
         for (Bike bike : garage.list()) {
             LOG.info("Creating daily recommendation check task for bikeId {}", bike.getId());
+            String taskName = "bikeId-" + bike.getId();
             try {
-                queue.add(TaskOptions.Builder
+                TaskHandle handle = queue.add(TaskOptions.Builder
                         .withUrl("/tasks/daily-recommendation-check")
-                        .taskName("bikeId-" + bike.getId())
+                        .taskName(taskName)
                         .param("bikeId", bike.getId())
                 );
+                LOG.info("A new task was added to the queue: {}", handle);
             } catch (TaskAlreadyExistsException e) {
-                LOG.error("Cannot generate daily recommendation check for " + bike.getId(), e);
+                LOG.error(new ParameterizedMessage("Task already exists {}, deleting it", taskName), e);
+                if (queue.deleteTask(taskName)) {
+                    LOG.info("Deleted task: {}", taskName);
+                } else {
+                    LOG.error("Cannot delete task: {}", taskName);
+                }
             }
 
         }
