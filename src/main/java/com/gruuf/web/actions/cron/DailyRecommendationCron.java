75fa1f9b90076d17c6cec02431f10dfcd8256ce9
Lukasz Lenart <lukasz.lenart@gmail.com>
2017-11-03 13:23:05 +0100

    Switches to per user check generation

--- src/main/java/com/gruuf/web/actions/cron/DailyRecommendationCron.java
+++ src/main/java/com/gruuf/web/actions/cron/DailyRecommendationCron.java
@@ -6,8 +6,9 @@ import com.google.appengine.api.taskqueue.TaskAlreadyExistsException;
 import com.google.appengine.api.taskqueue.TaskHandle;
 import com.google.appengine.api.taskqueue.TaskOptions;
 import com.gruuf.auth.Anonymous;
-import com.gruuf.model.Bike;
+import com.gruuf.model.User;
 import com.gruuf.services.Garage;
+import com.gruuf.services.UserStore;
 import com.gruuf.web.GruufAuth;
 import com.gruuf.web.actions.BaseAction;
 import com.opensymphony.xwork2.inject.Inject;
@@ -15,6 +16,8 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.message.ParameterizedMessage;
 import org.apache.struts2.convention.annotation.Result;
+
+import java.util.List;
 
 import static com.opensymphony.xwork2.Action.SUCCESS;
 
@@ -24,31 +27,34 @@ public class DailyRecommendationCron extends BaseAction {
 
     private static final Logger LOG = LogManager.getLogger(DailyRecommendationCron.class);
 
+    private UserStore userStore;
     private Garage garage;
 
     public String execute() {
         Queue queue = QueueFactory.getDefaultQueue();
-        
+
         LOG.info("Purging old tasks");
         queue.purge();
 
-        for (Bike bike : garage.list()) {
-            if(bike.getOwner().isNotify()) {
-                createTask(queue, bike);
+        List<User> users = userStore.list();
+
+        for (User user : users) {
+            if (user.isNotify() & garage.findByOwner(user).size() > 0) {
+                createTask(queue, user);
             }
         }
 
         return SUCCESS;
     }
 
-    private void createTask(Queue queue, Bike bike) {
-        LOG.info("Creating daily recommendation check task for bikeId {}", bike.getId());
-        String taskName = "bikeId-" + bike.getId() + "-" + GruufAuth.generateUUID();
+    private void createTask(Queue queue, User user) {
+        LOG.info("Creating daily recommendation check task for user {}", user.getId());
+        String taskName = "userId-" + user.getId() + "-" + GruufAuth.generateUUID();
         try {
             TaskHandle handle = queue.add(TaskOptions.Builder
                     .withUrl("/tasks/daily-recommendation-check")
                     .taskName(taskName)
-                    .param("bikeId", bike.getId())
+                    .param("userId", user.getId())
             );
             LOG.info("A new task was added to the queue: {}", handle);
         } catch (TaskAlreadyExistsException e) {
@@ -59,6 +65,11 @@ public class DailyRecommendationCron extends BaseAction {
                 LOG.error("Cannot delete task: {}", taskName);
             }
         }
+    }
+
+    @Inject
+    public void setUserStore(UserStore userStore) {
+        this.userStore = userStore;
     }
 
     @Inject
