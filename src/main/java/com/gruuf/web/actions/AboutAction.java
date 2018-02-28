f34684d9b9a234c9c5c076dc97086a6c8d13d638
Lukasz Lenart <lukasz.lenart@gmail.com>
2018-02-28 12:05:06 +0100

    Fixes how domain is calculated locally

--- src/main/java/com/gruuf/web/actions/AboutAction.java
+++ src/main/java/com/gruuf/web/actions/AboutAction.java
@@ -5,7 +5,6 @@ import com.gruuf.auth.Anonymous;
 import com.opensymphony.xwork2.inject.Inject;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.struts2.StrutsConstants;
 import org.apache.struts2.convention.annotation.Result;
 import org.apache.struts2.interceptor.ServletResponseAware;
 
@@ -20,11 +19,13 @@ public class AboutAction extends BaseAction implements ServletResponseAware {
 
     private static final Logger LOG = LogManager.getLogger(AboutAction.class);
 
+    @Inject(GruufConstants.HOST_URL)
     private String hostUrl;
+
     private HttpServletResponse response;
 
     @Override
-    public String execute() throws Exception {
+    public String execute() {
         Cookie cookie = createCookie();
 
         LOG.debug("Adds cookie: {}", cookie);
@@ -39,7 +40,7 @@ public class AboutAction extends BaseAction implements ServletResponseAware {
         cookie.setPath("/");
         int begin = hostUrl.lastIndexOf("://") + 3;
         int end = hostUrl.substring(begin).lastIndexOf(':');
-        String domain = hostUrl.substring(begin, end == -1 ? hostUrl.length() : end);
+        String domain = hostUrl.substring(begin, end == -1 ? hostUrl.length() : begin + end);
         LOG.debug("Cookie domain: {}", domain);
         cookie.setDomain(domain);
         return cookie;
@@ -48,11 +49,6 @@ public class AboutAction extends BaseAction implements ServletResponseAware {
     @Override
     public void setServletResponse(HttpServletResponse response) {
         this.response = response;
-    }
-
-    @Inject(GruufConstants.HOST_URL)
-    public void setHostUrl(String hostUrl) {
-        this.hostUrl = hostUrl;
     }
 
 }
